2489139c26fe0941dbb2aa5847a7419ef4027f1d
TimaPaps <ptv.devstudio@gmail.com>
2020-06-04 13:19:05 +0500

    дз урок 11 блок 2: JS: , animate, плавный скрол. Добавил пункт меню в header - Вопросы и ответы - с событием click и скролом до блока faq, добавил блок faq с содержимым с собитиями click и animate, добавил блок cookies с собитиями click и animate при нажатии на кнопку и крестик, добавил фикс стрелку вверх с собитиями click и animate, добавил плавный скрол при клике на пункт меню в header - О нас

--- script.js
+++ script.js
@@ -1,13 +1,58 @@
-$('.form-open').click(function(e){
+//вызов и скрытие формы сообщения
+$('.header-navigation-form-open, .main-form-open-btn, #picture-text-button').click(function(e){
     e.preventDefault();
-    console.log('click');
-    $(this).parents('.wrapper').find('.form-message').css('display', 'flex').hide().fadeIn(2000);
+    
+    $(this).parents('.wrapper').find('.form-message').css('display', 'flex').hide().fadeIn(750);
 });
 
 $('.form-message-block-button').click(function(){
-    $(this).parents('.form-message').fadeOut(2000);
+    $(this).parents('.form-message').fadeOut(750);
 });
+//конец вызова и скрытия формы сообщения
 
-$('.main-form-open-btn, #picture-text-button').click(function(){
-    $(this).parents('.wrapper').find('.form-message').css('display', 'flex').hide().fadeIn(2000);
+//анимация стрелки вверх и скрол страницы вверх
+$('.up-arrow').animate({
+    'right': '30px',
+    'bottom': '95px'
+}, 1000);
+
+$('.up-arrow').click(function(){
+    $('html, body').animate({
+        scrollTop: 0
+    }, 750);
 });
+//конец анимации стрелки вверх и скрола страницы вверх
+
+//скрол страницы вниз до до блока blocks при клике на Вопросы и ответы
+$('#header-navigation-about-us').click(function(e){
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $('.blocks').offset().top - $('.header').outerHeight()
+    }, 750);
+});
+//конец скрола страницы вниз до до блока blocks при клике на Вопросы и ответы
+
+//вызов и скрытие поля ответа в faq
+$('.faq-item-question').click(function(){
+    $(this).parents('.faq-item').find('.faq-item-answer').slideToggle(400);
+});
+//конец вызова и скрытия поля ответа в faq
+
+//скрол страницы вниз до до блока faq при клике на Вопросы и ответы
+$('.header-navigation-question-answer').click(function(e){
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $('.faq').offset().top - $('.header').outerHeight()
+    }, 750);
+});
+//конец скрола страницы вниз до до блока faq при клике на Вопросы и ответы
+
+//анимация cookies
+$('.cookies-btn, .cookies-btn-close').click(function(){
+    $(this).parents('.cookies').animate({
+        'bottom': '-85px'
+    }, 750);
+});
+//конец анимации cookies
